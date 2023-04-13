83651fca757940e0eb5b90a74c7bda2dab83b87b
Roberto Banić <roberto@banic.hr>
2023-04-14 00:28:01 +0200

    Bump minimum Python version to 3.9

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
 
 if __name__ == "__main__":
-    # Check python version 3.6 or higher
-    if not (3, 6) <= tuple(map(int, sys.version_info[:2])):
-        print("This script requires Python 3.6 or higher")
+    # Check python version 3.9 or higher
+    if not (3, 9) <= tuple(map(int, sys.version_info[:2])):
+        print("This script requires Python 3.9 or higher")
         sys.exit(1)
 
     from src.main import main
